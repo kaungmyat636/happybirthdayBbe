4770174bc1f312fae717d23dbd46d7dad31ca23a
kaungmyat636 <km6355183@gmail.com>
2026-06-21 14:54:12 +0630

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@
 const CONFIG = {
   // ── Recipient Info ────────────────────────────────────────────
   name: "Baby",
-  photo: "./img/mygirl.jpg",       // Place your photo in the img/ folder
+  photo: "./mygirl.jpg",       // Place your photo in the img/ folder
   music: "./music/hbd.mpeg",      // Place your music in the music/ folder
 
   // ── Theme Colors ──────────────────────────────────────────────
